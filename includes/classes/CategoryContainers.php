e3b737e96a5d29540176c3a9a5709f02367caa75
hding2196 <hding2196@gmail.com>
2022-05-23 17:02:29 +0700

    update category data in home page and style

--- includes/classes/CategoryContainers.php
+++ includes/classes/CategoryContainers.php
@@ -15,14 +15,36 @@ class CategoryContainers {
         $html = "<div class='previewCategories'>";
 
         while($row = $query->fetch(PDO::FETCH_ASSOC)) {
-            $html .= $row["name"];
+            $html .= $this->getCategoryHtml($row, null, true, true);
         }
 
         return $html . "</div>";
     }
 
-    private function getCategoryHtml($sqlData) {
+    private function getCategoryHtml($sqlData, $title, $tvShows, $movies) {
+        $categoryId = $sqlData["id"];
+        $title = $title == null ? $sqlData["name"] : $title;
 
+        if($tvShows && $movies) {
+            $enntities = EntityProvider::getEntities($this->con, $categoryId
+            , 30);
+        } else if ($tvShows) {
+            // Get tv show entities
+        } else {
+            // Get movie entities
+        }
+
+        if(sizeof($enntities) == 0) {
+            return;
+        }
+
+        $entitiesHtml = "";
+        $previewProvider = new PreviewProvider($this->con, $this->username);
+        foreach($enntities as $entity) {
+            $entitiesHtml .= $previewProvider->createEntityPreviewSquare($entity);
+        }
+
+        return $entitiesHtml . "</br>";
     }
 }
 ?>
--- includes/header.php
+++ includes/header.php
@@ -3,6 +3,7 @@ require_once("./includes/config.php");
 require_once("./includes/classes/PreviewProvider.php");
 require_once("./includes/classes/CategoryContainers.php");
 require_once("includes/classes/Entity.php");
+require_once("includes/classes/EntityProvider.php");
 
 
 if(!isset($_SESSION["userLoggedIn"])) {
