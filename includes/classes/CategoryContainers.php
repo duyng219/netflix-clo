5eb17cd57ec3de20939cf23d1b8f1b9a7796f800
hding2196 <hding2196@gmail.com>
2022-05-23 17:29:34 +0700

    update style category and entity in home page

--- includes/classes/CategoryContainers.php
+++ includes/classes/CategoryContainers.php
@@ -44,7 +44,14 @@ class CategoryContainers {
             $entitiesHtml .= $previewProvider->createEntityPreviewSquare($entity);
         }
 
-        return $entitiesHtml . "</br>";
+        return "<div class='category'>
+                    <a href='category.php?id=$categoryId'>
+                        <h3>$title</h3>
+                    </a>
+                    <div class='entities'>
+                        $entitiesHtml
+                    </div>
+                </div>";
     }
 }
 ?>
