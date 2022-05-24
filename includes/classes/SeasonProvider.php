77091c6f6b38efa919d0fb250ec0fa5dd29faca2
hding2196 <hding2196@gmail.com>
2022-05-24 16:10:48 +0700

    create view video of season in entity page

--- includes/classes/SeasonProvider.php
+++ includes/classes/SeasonProvider.php
@@ -9,6 +9,48 @@
 
         public function create($entity) {
             $seasons = $entity->getSeasons();
+
+            if(sizeof($seasons) == 0) {
+                return;
+            }
+    
+            $seasonsHtml = "";
+            foreach($seasons as $season) {
+                $seasonNumber = $season->getSeasonNumber();
+
+                $videosHtml = "";
+                foreach($season->getVideos() as $video) {
+                    $videosHtml .= $this->createVideoSquare($video);
+                }
+    
+    
+                $seasonsHtml .= "<div class='season'>
+                                        <h3>Season $seasonNumber</h3>
+                                        <div class='videos'>
+                                            $videosHtml
+                                        </div>
+                                    </div>";
+            }
+    
+            return $seasonsHtml;
+        }
+
+        private function createVideoSquare($video) {
+            $id = $video->getId();
+            $thumbnail = $video->getThumbnail();
+            $name = $video->getTitle();
+            $description = $video->getDescription();
+            $episodeNumber = $video->getEpisodeNumber();
+            
+            return "<a href='watch.php?id=$id'>
+                        <div class='contents'>
+                            <img src='$thumbnail' />
+                            <div class='videoInfo'>
+                                <h4>$name</h4>
+                                <span>$description</span>
+                            </div>
+                        </div>
+                    </a>";
         }
     }
 ?>
--- includes/classes/Video.php
+++ includes/classes/Video.php
@@ -18,5 +18,29 @@
 
             $this->entity = new Entity($con, $this->sqlData["entityId"]);
         }
+
+        public function getId() {
+            return $this->sqlData["id"];
+        }
+
+        public function getTitle() {
+            return $this->sqlData["title"];
+        }
+
+        public function getDescription() {
+            return $this->sqlData["description"];
+        }
+
+        public function getFilePath() {
+            return $this->sqlData["filePath"];
+        }
+        
+        public function getThumbnail() {
+            return $this->entity->getThumbnail();
+        }
+
+        public function getEpisodeNumber() {
+            return $this->sqlData["episode"];
+        }
     }
 ?>
